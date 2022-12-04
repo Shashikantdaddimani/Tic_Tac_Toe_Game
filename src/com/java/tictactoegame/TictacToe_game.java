cc35100594843c43af04c1e939de465f98a563d8
Shashikantdaddimani <shashi0344daddimani@gmail.com>
2022-12-04 09:27:00 +0530

    Added:Logic for user to make a move to a desired location in the board

--- src/com/java/tictactoegame/TictacToe_game.java
+++ src/com/java/tictactoegame/TictacToe_game.java
@@ -3,7 +3,7 @@ package com.java.tictactoegame;
 /**
  * imports all the class of the java.util package
  */
-import java.util.Scanner;
+import java.util.*;
 
 /**
  * Class to create TicTacToe game with different methods
@@ -14,11 +14,11 @@ public class TictacToe_game {
 	 */
 	static char[] board = new char[10];
 	static char player, computer;
+	private static int playLocation;
 	static Scanner input = new Scanner(System.in);
 
 	/*
-	 * UC1 Name: createEmptyBoard 
-	 * Description: Static method to create empty board
+	 * UC1 Name: createEmptyBoard Description: Static method to create empty board
 	 * Algorithm: It takes board array indexes from 1 to 9 using for loop and assign
 	 * empty space to each element in the board.
 	 */
@@ -30,25 +30,25 @@ public class TictacToe_game {
 	}
 
 	/**
-	 * UC2 Name: chooseLetter
+	 * UC2 Name: chooseLetter 
 	 * Description: static method to choose letter for user
 	 * and computer
-	 * 
 	 */
 	public static void chooseLetter() {
-		System.out.println("Please Select Your Choice Letter : 'X' or 'O' ");
-		char choice = input.next().charAt(0);
-		if (choice == 'X') {
-			player = 'X';
+
+		System.out.print("select X or O : ");
+		player = Character.toUpperCase(input.next().charAt(0));
+		if (player == 'X')
 			computer = 'O';
-		} else if (choice == 'Y') {
-			player = 'O';
+		else
 			computer = 'X';
-		}
+		System.out.println("You have selected : " + player);
+		System.out.println("Computer's choice is : " + computer);
 	}
 
 	/*
-	 * UC3 Method Name: showBoard 
+	 * UC3 
+	 * Method Name: showBoard 
 	 * Description: method to display current board
 	 * Algorithm: it prints the every element in board array using println method.
 	 */
@@ -59,6 +59,26 @@ public class TictacToe_game {
 		System.out.println("---------");
 		System.out.println(board[7] + " | " + board[8] + " | " + board[9]);
 	}
+	 /*
+     * UC4
+     * Method Name: makeMove
+     * Description: method to make user to move to desired location
+     * Algorithm: It takes user input from index 1 to 9. If you enter invalid index or try to move
+     * to occupied position it prints error message else it moves the user letter to desired location.
+     */
+
+	private static void makeMove() {
+		System.out.println("Enter Location 1-9 to Make Move");
+		playLocation = input.nextInt();
+		if (playLocation < 10 && playLocation > 0) {
+			board[playLocation] = player;
+			showBoard();
+			makeMove();
+
+		} else {
+			System.out.println("Invalid Choice");
+		}
+	}
 
 	/*
 	 * Program execution starts from main method
@@ -67,6 +87,7 @@ public class TictacToe_game {
 		createEmptyBoard();
 		chooseLetter();
 		showBoard();
+		makeMove();
 	}
 
 }
